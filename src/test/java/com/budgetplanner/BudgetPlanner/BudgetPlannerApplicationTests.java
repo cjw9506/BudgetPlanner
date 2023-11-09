4e948bb180bbf24de9d9e53ebc54782162d1c88f
jeongjiwon <jungkt0x@naver.com>
2023-11-09 18:12:27 +0900

    feat: 초기 설정 추가
    프로젝트 기본 세팅 추가

---
+++ src/test/java/com/budgetplanner/BudgetPlanner/BudgetPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetplanner.BudgetPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
