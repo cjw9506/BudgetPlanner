faf098e6c50b1bde0a1c7b8dfeca5d5e095f63a3
jeongjiwon <jungkt0x@naver.com>
2023-11-14 16:51:28 +0900

    test: 로그인 및 토큰 재발급 테스트 추가
    - controller 단 테스트 추가
    - service 단 테스트 추가

--- src/test/java/com/budgetplanner/BudgetPlanner/auth/service/AuthServiceTest.java
+++ src/test/java/com/budgetplanner/BudgetPlanner/auth/service/AuthServiceTest.java
@@ -1,6 +1,8 @@
 package com.budgetplanner.BudgetPlanner.auth.service;
 
+import com.budgetplanner.BudgetPlanner.auth.dto.UserLoginRequest;
 import com.budgetplanner.BudgetPlanner.auth.dto.UserSignupRequest;
+import com.budgetplanner.BudgetPlanner.auth.jwt.JwtUtils;
 import com.budgetplanner.BudgetPlanner.common.exception.CustomException;
 import com.budgetplanner.BudgetPlanner.user.entity.User;
 import com.budgetplanner.BudgetPlanner.user.repository.UserRepository;
@@ -27,6 +29,9 @@ class AuthServiceTest {
 
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private JwtUtils jwtUtils;
 
     @Spy
     private PasswordEncoder passwordEncoder;
@@ -70,5 +75,94 @@ class AuthServiceTest {
         assertThrows(CustomException.class, () -> authService.userSignup(request));
 
         verify(userRepository, never()).save(any());
+    }
+
+    @DisplayName("로그인 성공")
+    @Test
+    void login_Success() {
+        // Arrange
+        String account = "testAccount";
+        String password = "password123";
+        String encodedPassword = "encodedPassword123";
+        String[] tokens = new String[]{"access-token", "refresh-token"};
+
+        UserLoginRequest request = UserLoginRequest.builder()
+                .account(account)
+                .password(password)
+                .build();
+
+        User user = User.builder()
+                .account(account)
+                .password(encodedPassword)
+                .build();
+
+        //given
+        when(userRepository.findByAccount(account)).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(password, encodedPassword)).thenReturn(true);
+        when(jwtUtils.generateToken(account)).thenReturn(tokens);
+
+        //when
+        String[] result = authService.userLogin(request);
+
+        //then
+        assertEquals(tokens[0], result[0]);
+        assertEquals(tokens[1], result[1]);
+    }
+
+    @DisplayName("로그인 시 유저 못찾음")
+    @Test
+    void login_UserNotFound() {
+        String account = "testAccount";
+        String password = "password123";
+
+        UserLoginRequest request = UserLoginRequest.builder()
+                .account(account)
+                .password(password)
+                .build();
+
+        //given
+        when(userRepository.findByAccount(account)).thenReturn(Optional.empty());
+
+        //then
+        assertThrows(CustomException.class, () -> authService.userLogin(request));
+    }
+
+    @DisplayName("로그인시 패스워드 일치하지 않음")
+    @Test
+    void login_PasswordMismatch() {
+        String account = "testAccount";
+        String password = "password123";
+        String encodedPassword = "encodedPassword123";
+
+        UserLoginRequest request = UserLoginRequest.builder()
+                .account(account)
+                .password(password)
+                .build();
+
+        User user = User.builder()
+                .account(account)
+                .password(encodedPassword)
+                .build();
+
+        //given
+        when(userRepository.findByAccount(account)).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(password, encodedPassword)).thenReturn(false);
+
+        //then
+        assertThrows(CustomException.class, () -> authService.userLogin(request));
+    }
+
+    @DisplayName("토큰 재발급")
+    @Test
+    void issueNewAccessToken() {
+
+        String refreshToken = "refresh-token";
+        String newAccessToken = "new-access-token";
+
+        when(jwtUtils.verifyRefreshTokenAndReissue(refreshToken)).thenReturn(newAccessToken);
+
+        String result = authService.issueRefreshToken(refreshToken);
+
+        assertEquals(newAccessToken, result);
     }
 }
