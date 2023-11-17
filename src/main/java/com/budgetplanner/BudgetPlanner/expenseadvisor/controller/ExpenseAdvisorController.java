8bbfbfefd81e463c4267bd8b9b512cdfb4176dcc
jeongjiwon <jungkt0x@naver.com>
2023-11-17 23:39:58 +0900

    feat: 오늘 예산 추천 기능 구현

---
+++ src/main/java/com/budgetplanner/BudgetPlanner/expenseadvisor/controller/ExpenseAdvisorController.java
@@ -0,0 +1,29 @@
+package com.budgetplanner.BudgetPlanner.expenseadvisor.controller;
+
+import com.budgetplanner.BudgetPlanner.expenseadvisor.dto.BudgetRecommendationResponse;
+import com.budgetplanner.BudgetPlanner.expenseadvisor.service.ExpenseAdvisorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/expense-advisor")
+public class ExpenseAdvisorController {
+
+    private final ExpenseAdvisorService expenseAdvisorService;
+
+    @GetMapping("/recommend")
+    public ResponseEntity<?> recommend(Authentication authentication) {
+
+        BudgetRecommendationResponse response = expenseAdvisorService.getRecommendation(authentication);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+
+}
