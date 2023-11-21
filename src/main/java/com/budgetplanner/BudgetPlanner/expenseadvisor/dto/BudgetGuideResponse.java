c8efe52ef254a188f52944d84dec5620a83053ca
jeongjiwon <jungkt0x@naver.com>
2023-11-22 01:12:26 +0900

    feat: 디스코드 웹훅 메시지 생성

--- src/main/java/com/budgetplanner/BudgetPlanner/expenseadvisor/dto/BudgetGuideResponse.java
+++ src/main/java/com/budgetplanner/BudgetPlanner/expenseadvisor/dto/BudgetGuideResponse.java
@@ -12,16 +12,53 @@ import java.util.Map;
 public class BudgetGuideResponse {
 
     private int todaySpentAmount;
-    private Map<Category, Double> todayCategorySpent;
+    private Map<Category, Integer> todayCategorySpent;
     private Map<Category, Integer> categoryBudgets;
-    private Map<Category, Integer> risk;
+    private Map<Category, String> risk;
 
     @Builder
-    public BudgetGuideResponse(int todaySpentAmount, Map<Category, Double> todayCategorySpent,
-                               Map<Category, Integer> categoryBudgets, Map<Category, Integer> risk) {
+    public BudgetGuideResponse(int todaySpentAmount, Map<Category, Integer> todayCategorySpent,
+                               Map<Category, Integer> categoryBudgets, Map<Category, String> risk) {
         this.todaySpentAmount = todaySpentAmount;
         this.todayCategorySpent = todayCategorySpent;
         this.categoryBudgets = categoryBudgets;
         this.risk = risk;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("💰 **예산 관리 알림** 💰\n");
+        result.append("오늘의 지출: ").append(todaySpentAmount).append("원\n");
+        result.append("카테고리별 지출:\n");
+
+        for (Map.Entry<Category, Integer> entry : todayCategorySpent.entrySet()) {
+            String categoryName = entry.getKey().name(); // 카테고리 이름
+            int spentAmount = entry.getValue(); // 해당 카테고리의 지출 금액
+
+            result.append("  • ").append(categoryName).append(": ").append(spentAmount).append("원\n");
+        }
+
+        result.append("\n예산 상태:\n");
+
+        for (Map.Entry<Category, Integer> entry : categoryBudgets.entrySet()) {
+            String categoryName = entry.getKey().name(); // 카테고리 이름
+            int budgetAmount = entry.getValue(); // 해당 카테고리의 예산
+
+            result.append("  • ").append(categoryName).append(" 예산: ").append(budgetAmount).append("원\n");
+        }
+
+        result.append("\n리스크 분석:\n");
+
+        for (Map.Entry<Category, String> entry : risk.entrySet()) {
+            String categoryName = entry.getKey().name(); // 카테고리 이름
+            String riskPercentage = entry.getValue(); // 해당 카테고리의 리스크
+
+            result.append("  • ").append(categoryName).append(" 리스크: ").append(riskPercentage).append("\n");
+        }
+
+        result.append("\nEnjoy your budgeting! 💸");
+
+        return result.toString();
     }
 }
