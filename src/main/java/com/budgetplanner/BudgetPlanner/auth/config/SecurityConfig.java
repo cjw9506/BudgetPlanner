aa2c9ae6942809fd6ad46a9c4c007d6ec8c97359
jeongjiwon <jungkt0x@naver.com>
2024-12-04 17:07:22 +0900

    fix : security url 수정

--- src/main/java/com/budgetplanner/BudgetPlanner/auth/config/SecurityConfig.java
+++ src/main/java/com/budgetplanner/BudgetPlanner/auth/config/SecurityConfig.java
@@ -29,8 +29,8 @@ public class SecurityConfig {
 
         return http
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers(POST, "/hc").permitAll()
-                        .requestMatchers(POST, "/env").permitAll()
+                        .requestMatchers(GET, "/hc").permitAll()
+                        .requestMatchers(GET, "/env").permitAll()
                         .requestMatchers(POST, "/api/signup").permitAll()
                         .requestMatchers(POST, "/api/login").permitAll()
                         .requestMatchers(POST, "/api/refresh").permitAll()
